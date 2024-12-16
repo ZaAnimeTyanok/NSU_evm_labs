a3f7ee7422a0f72d9577c45acba4f650d3df8cc0
Yanchenko Sergey <144523679+ZaAnimeTyanok@users.noreply.github.com>
2024-12-16 16:11:19 +0700

    Create main.c

---
+++ lab9/main.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <immintrin.h>
+#include <inttypes.h>
+#include <limits.h>
+
+
+const int N = 100000000 * 2;
+const int COUNT = 10;
+
+
+unsigned long long tacts(int* arr) {
+    unsigned long long start, end, min_time = ULLONG_MAX;
+
+    for (int j = 0; j < COUNT; j++) {
+        start = __rdtsc();
+        volatile int k = 0;
+
+        for (int i = 0; i < N; i++) {
+            k = arr[k];
+        }
+
+        end = __rdtsc();
+
+        if (min_time > end - start) {
+            min_time = end - start;
+        }
+    }
+
+    return min_time / N;
+}
+
+void fill_array(int* arr, int fragments, int offset, int size) {
+    for (int i = 0; i < size; i++) {
+        for (int j = 1; j < fragments; j++) {
+            arr[i + (j - 1) * offset] = i + j * offset;
+        }
+
+        arr[i + (fragments - 1) * offset] = i + 1;
+    }
+
+    arr[size - 1 + (fragments - 1) * offset] = 0;
+}
+
+const int FRAGMENT_SIZE = 1 * 1024 * 1024; 
+const int OFFSET_SIZE = 1 * 1024 * 1024; 
+
+int main() {
+    int* arr = (int*)calloc(N, sizeof(int));
+
+    if (arr == NULL) {
+        fprintf(stderr, "Memory allocation failed for N: %d\n", N);
+        exit(EXIT_FAILURE);
+    }
+
+    FILE* out = fopen("result.csv", "w");
+
+    fprintf(out, "fragments,tacts\n");
+
+    for (int n = 1; n <= 64; n++) {
+        fill_array(arr, n, OFFSET_SIZE / sizeof(int), FRAGMENT_SIZE / sizeof(int));
+
+        fprintf(out, "%d,%llu\n", n, tacts(arr));
+    }
+
+    fclose(out);
+
+    free(arr);
+
+    return 0;
+}
